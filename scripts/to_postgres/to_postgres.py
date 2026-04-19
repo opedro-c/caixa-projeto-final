1cf3bc9d28a7623bed5428723b712f6898bad479
Pedro Costa <pedroc.aragao@outlook.com>
2026-04-19 19:53:18 -0300

    e lá vamos nós

--- scripts/to_postgres/to_postgres.py
+++ scripts/to_postgres/to_postgres.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-# 1. Configurações de Conexão (Ajuste com seus dados)
 DB_USER = 'postgres'
 DB_PASSWORD = 'postgres'
 DB_HOST = 'postgres'
